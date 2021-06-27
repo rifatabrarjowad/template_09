8c2919f935e77bd5a8cfbc8b4705bdb492a34cf9
RIFAT ABRAR JOWAD <rajdiprifat2004@gmail.com>
2021-06-27 20:54:00 +0600

    adding file

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const plas = document.getElementById('plas');
 plas.addEventListener('click', function () {
     const one = document.getElementById('one').value;
     const oneN = parseFloat(one)
-    const oneA = oneN + 1;
-    console.log(oneA);
+    const oneA = oneN +1;
+    console.log(`oneA ${oneA}`);
     document.getElementById('one').value = oneA;
+    let add = document.getElementById('add').innerText;
+    let adds =parseFloat(add);
+    let addss = oneA * 1219;
+    document.getElementById('add').innerText = addss;
+    console.log(`adds ${adds}`)
+    
+const product2 = document.getElementById('product2').innerText;
+const product2Float = parseFloat(product2);
+console.log(`product2 =${product2} and addss =${addss}`)
+const total = addss + product2Float;
 
-
-    // .........
-    const onee = document.getElementById('sub').innerText;
-
-    const oneeN = parseFloat(onee)
-    console.log(oneeN);
-    const oneeA = oneeN + 1219;
-    console.log(oneeA);
-    document.getElementById('sub').innerText = oneeA;
-    document.getElementById('sub2').innerText = oneeA;
-
+document.getElementById('sub').innerText=total;
+document.getElementById('sub2').innerText=total;
 })
 const mainus = document.getElementById('mainus');
 mainus.addEventListener('click', function () {
@@ -25,6 +26,10 @@ mainus.addEventListener('click', function () {
     const oneA = oneN - 1;
     console.log(oneA);
     document.getElementById('one').value = oneA;
+    const delet = document.getElementById('add').innerText;
+    let delets =parseFloat(delet);
+    let final = delets -1219;
+    document.getElementById('add').innerText = final;
 })
 
 const pos = document.getElementById('pos');
@@ -68,3 +73,4 @@ dtf.addEventListener('click', function () {
     document.getElementById('sub2').innerText = 0;
 
 })
+
